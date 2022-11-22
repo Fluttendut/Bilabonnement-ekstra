00d59cf6b4cd5eefe288d3b8b2fe31bdcba21456
Zouthen <mich5n69@stud.kea.dk>
2022-11-22 14:56:39 +0100

    DB Connection

---
+++ src/main/java/com/example/bilabonnement/repositories/DatabaseConnectionManager.java
@@ -0,0 +1,42 @@
+package com.example.bilabonnement.repositories;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnectionManager {
+    //variabler
+    private static String hostname;
+    private static String username;
+    private static String password;
+    private static Connection conn;
+
+    //metoder
+    public static Connection getConnection() throws IOException
+    {
+        InputStream propertiesFiles = new FileInputStream("src/main/resources/application.properties");
+
+        Properties properties = new Properties();
+        properties.load(propertiesFiles);
+        hostname = properties.getProperty("spring.datasource.url");
+        username = properties.getProperty("spring.datasource.username");
+        password = properties.getProperty("spring.datasource.password");
+
+
+        try
+        {
+            conn = DriverManager.getConnection(hostname,username, password);
+        }
+        catch (SQLException e){
+            System.out.println("connection to database failed!");
+            throw new RuntimeException(e);
+        }
+        System.out.println("connection to database successfull ");
+        return conn;
+    }
+}
